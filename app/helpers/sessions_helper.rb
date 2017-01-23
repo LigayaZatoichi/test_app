9e43b966598bc7d2fdd0050cb2439a06d73ea6c8
Dave <braddahdave@arrayschool.com>
2017-01-23 15:33:50 -0700

    User security+

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,10 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
 # Returns the current logged-in user (if any)
   def current_user
     if (user_id = session[:user_id])
