e46c7e93f7b8b27de55e8b16c0be6a3c007aa86e
Dave <braddahdave@arrayschool.com>
2017-01-24 16:39:51 -0700

    added some tests or something (i'm drunk)

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,9 +18,26 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
-    log_in_as(@user)
+    log_in_as(@user) # Log in as a valid user.
+    get edit_user_path(@user) # Navigate to the user_edit page.
+    assert_template 'users/edit' #Double-checking to see if we are actually on the edit page for users.
+    name = "Foo Bar" # Local variables to be used later.
+    email = "foo@bar.com" # Local variables to be used later.
+    patch user_path(@user), params: { user: {name: name,
+                                            email: email,
+                                            password: "",
+                                            password_confirmation: "" } } # Change the users name and email, then hit submit.
+    assert_not flash.empty? # Making sure there is a flash.
+    assert_redirected_to @user # Redirecting to the user's profile.
+    @user.reload # Reloads the users default page and new patch data.
+    assert_equal name, @user.name # Double-checking the users name is equal to the patch.
+    assert_equal email, @user.email # Double-checking the users email is equal to the patch.
+  end
+
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: {name: name,
