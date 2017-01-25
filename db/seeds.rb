148cdf92ddc2585abd7bb61565ebf6d49d7598cd
Dave <braddahdave@arrayschool.com>
2017-01-25 16:19:44 -0700

    email activation continuing

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,18 @@ User.create!(name: "Dave Ewaliko",
             email: "braddahdave@arrayschool.com",
             password: "password",
             password_confirmation: "password",
-            admin: true)
+            admin: true,
+            activated: true,
+            activated_at: Time.zone.now)
 
 99.times do |n|
-  name = Faker::Name.name
-  email = "example-#{n+1}@arrayschool.com"
+  name = Faker::Superhero.name
+  email = Faker::Internet.email(name)
   password = "password"
   User.create!(name: name,
                email: email,
                password: "password",
-               password_confirmation: "password")
+               password_confirmation: "password",
+               activated: true,
+               activated_at: Time.zone.now)
 end
