4a491cf871ad4aa95dfa0d02cc48cc9696488c80
Dave <braddahdave@arrayschool.com>
2017-01-25 14:18:20 -0700

    Finished up users resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 User.create!(name: "Dave Ewaliko",
             email: "braddahdave@arrayschool.com",
             password: "password",
-            password_confirmation: "password")
+            password_confirmation: "password",
+            admin: true)
 
 99.times do |n|
   name = Faker::Name.name
